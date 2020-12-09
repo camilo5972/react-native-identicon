ab0c8dd706be372404d0edc744c7bd949f378e73
Camilo Henao Navarro <camilohenao@MacBook-Pro-de-Camilo.local>
2020-12-09 12:47:49 -0500

    chore: first commit

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # react-native-identicon
+
+The core of the project is a clone of the repository: [node-identicon](https://github.com/Ajido/node-identicon). I only made the library wrapper as a component in addition to using other alternate libraries such as: react-native-crypto and react-native-canvas
+
+## Install
+
+```bash
+npm install react-native-identicon
+npx pod-install ios
+```
+
+## Usage
+
+```JSX
+import React from 'react';
+import Identicon from 'react-native-identicon';
+
+const App = () => {
+    return (
+        <Identicon value={'test'} />
+    )
+}
+```
+
+Optional parameters
+
+* style: object with container's styles
+* size: default 45
---
+++ src/index.tsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useRef } from 'react';
+import { View } from 'react-native';
+import Canvas from 'react-native-canvas';
+import { IndenticonLib } from './identicon';
+
+interface Props {
+    value: string;
+    style?: {[key: string]: any};
+    size?: number;
+}
+
+const Identicon = ({ value, style, size = 45 }: Props) => {
+    const handleCanvas = useRef(null);
+
+    useEffect(() => {
+        if (handleCanvas.current) IndenticonLib.generateSync(handleCanvas.current, { id: value, size }, size);
+    }, []);
+    
+    return (
+        <View style={[{width: size, height: size, borderRadius: 30, overflow: 'hidden'}, style]}>
+            <Canvas ref={handleCanvas}/>
+        </View>
+    );
+}
+
+export default Identicon;
